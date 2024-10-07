6cb7103f2cf7040855c4926b51b67ef63d82693c
Anant7252 <ananttripathi7252@gmail.com>
2024-10-07 15:31:03 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,24 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        a={
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-        b=0
-        c=len(s)
-        for i in range(len(s)):
-            if i+1< c and a[s[i]]<a[s[i+1]]:
-                b=a[s[i]]-1
-            else:
-                b=a[s[i]]+1
-
-        return  b
+        roman_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    length = len(s)
+    
+    # Iterate over each character in the string
+    for i in range(length):
+        # If the current numeral is smaller than the next one, subtract it
+        if i + 1 < length and roman_map[s[i]] < roman_map[s[i + 1]]:
+            total -= roman_map[s[i]]
+        else:
+            total += roman_map[s[i]]
+    
+    return total
 
 
 
