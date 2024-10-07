022c09e6ab84750d66bd28b039a9ebb046c4554f
Anant7252 <ananttripathi7252@gmail.com>
2024-10-07 15:31:09 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -12,20 +12,20 @@ class Solution(object):
         'C': 100,
         'D': 500,
         'M': 1000
-    }
-    
-    total = 0
-    length = len(s)
-    
-    # Iterate over each character in the string
-    for i in range(length):
-        # If the current numeral is smaller than the next one, subtract it
-        if i + 1 < length and roman_map[s[i]] < roman_map[s[i + 1]]:
-            total -= roman_map[s[i]]
-        else:
-            total += roman_map[s[i]]
-    
-    return total
+        }
+        
+        total = 0
+        length = len(s)
+        
+        # Iterate over each character in the string
+        for i in range(length):
+            # If the current numeral is smaller than the next one, subtract it
+            if i + 1 < length and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+        
+        return total
 
 
 
