b7da41edb15b5dc353cdb8ec23180d72f3461ec2
Anant7252 <ananttripathi7252@gmail.com>
2024-10-07 15:23:30 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        a={
+            'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+        }
 
 
 
